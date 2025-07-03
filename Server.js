0b6c25aa5f81468ae0de7196eaf11253c041ff4d
Vivek-7684 <vivek.infosware@gmail.com>
2025-07-03 12:49:26 +0530

    changes

--- Server.js
+++ Server.js
@@ -1,50 +1,79 @@
 const express = require('express');
 const connection = require('./db');
-const bodyParser = require('body-parser');
-const multer = require('multer');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 
-app.post("/addProduct", upload.single('file'), (req, res) => {
-    console.log(req);
-    // connection.query("Insert Into ecommerce Values(image,title,content,categories,price)", [], 
-    // )
-})
+app.post("/signup", async (req, res) => {
 
-app.get() // get data / filter data
-
-app.post("/signUp", (req, res) => {
-
-    console.log(req.body);
     const { username, email, password } = req.body;
-    console.log(e.target.Files[0]);
 
-    if (!username || !email || !password) {
-        return res.status(400).send({ message: "Please All Required Details" });
+    if (!validator.isAlpha(username.trim())) {  // remove white space in username from both side 
+        return res.status(400).send({ message: "Name must letter only" });
+    }
+    else if (!validator.isEmail(email)) {
+        return res.status(400).send({ message: "Email must have @ with domain and ." });
+    }
+    else if (!validator.isStrongPassword(password)) {  // check letter only
+        return res.status(400).send({ message: "Password must be minimum 8 characters with atleast one Capital and one small alphabet characters,one special characters and with number" });
     }
 
-    let smallAlpabet = false;
-    let capitalAlpabet = false;
-    let numberRange = false;
+    connection.query("Select * from ecom_user where username = ? or email = ?", [username, email], (error, result) => {
 
-    for (const ab of password) {
-        if (ab >= 'a' && ab <= 'z') {
-            smallAlpabet = true;
+        if (result.length > 0) {
+            return res.status(409).send({ message: "User Already Exist.Try with other username and email" });
         }
-        else if (ab >= 'A' && ab <= 'Z') {
-            capitalAlpabet = true;
-        }
-        else if ((ab >= 1 && ab <= 9)) {
-            numberRange = true;
-        }
-    }
 
-    if (smallAlpabet && capitalAlpabet && numberRange) return res.status(400).send({ message: "Password must contain atleast one small and capital characters and one number." });
+    })
 
-    connection.query("Select * from ecom_user where user")
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    connection.query("insert into ecom_user(username,email,password) values(?,?,?) ", [username, email, hashedpassword], (err) => {
+
+        if (err) {
+            return res.status(500).send({ message: "Database error" })
+        };
+
+        res.status(201).send({ message: "Created" });
+    })
+
 })
+
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!validator.isEmail(email)) {
+        return res.status(400).send({ message: "Email must have @ with domain and ." });
+    }
+    else if (!validator.isStrongPassword(password)) {  // check letter only
+        return res.status(400).send({ message: "Password must be minimum 8 characters with atleast one Capital and one small alphabet characters,one special characters and with number" });
+    }
+
+    connection.query("Select * from ecom_user where email = ?", [email], (error, result) => {
+        if (result.length === 0) {
+            return res.status(401).send({ message: "User does not exist.Please get Registered" });
+        };
+
+        bcrypt.compare(password, result[0].password).then((result) => {
+            if (result) {
+                res.status(200).send({ message: "Logged In" });
+            } else {
+                res.status(401).send({ message: "Wrong Crendentials" });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    })
+
+
+})
+
 
 app.listen(3001, (err) => {
     if (err) console.log(err)
