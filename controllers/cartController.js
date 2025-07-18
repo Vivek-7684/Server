d79127f2bfb0f8f681ab66d006830f5dfc19bf56
Vivek-7684 <vivek.infosware@gmail.com>
2025-07-18 23:50:31 +0530

    Fixes in get Cart Product and add to Cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,46 +4,54 @@ const productModel = require("../model/productModel");
 // get User's Product in Cart
 exports.getProductsInCart = (req, res) => {
     try {
-        const { userId } = req.query;
-        cartModel.getProductsInCart(userId, productId, (err, result) => {
-            try {
-                if (err) return res.status(500).send({ message: "Database Error" });
-                return res.status(200).send(result);
-            } catch (err) {
-                return res.status(500).send(err);
-            }
-        });
+        userModel.getUsernameByEmail(req.Email, (err, result) => {
+
+            if (err) return res.status(500).send(err.message);
+            if (result.length === 0) return res.status(404).send({ message: "User not found" });
+
+            const userId = result[0].id;
+            // get User's Product in Cart
+            cartModel.getProductsInCart(userId, (err, result) => {
+                try {
+                    if (err) return res.status(500).send({ message: "Database Error" });
+                    return res.status(200).send(result);
+                } catch (err) {
+                    return res.status(500).send(err.message);
+                }
+            });
+        })
+
     }
     catch (err) {
-        return res.status(500).send(err);
+        return res.status(500).send(err.message);
     }
 
 }
 
 // add Product to Cart
 exports.addProductToCart = (req, res) => {
-    console.log(req.Email);
+
 
     try {
         const { productId, quantity } = req.body;
-        console.log("quantity", quantity);
+
         userModel.getUsernameByEmail(req.Email, (err, result) => {
             if (err) return res.status(500).send(err.message);
             if (result.length === 0) return res.status(404).send({ message: "User not found" });
 
             const userId = result[0].id;
-            console.log("User ID:", userId);
 
             cartModel.getProductsInCart(userId, (err, result) => {
-                console.log("Cart Result:", result);
-                if (result.some(item => item.productId === productId)) { // check if product already exists in cart
-                    const existingProduct = result.find(item => item.productId === productId);
-                    console.log("Existing Product:", existingProduct);
-                    const updatedProductQuantity = parseInt((existingProduct.quantity || 0) + parseInt(quantity));
-                    console.log("Updated Product Quantity:", updatedProductQuantity);
+             
+                if (result.some(item => item.id === productId)) { // check if product already exists in cart
+                    
+                    const existingProduct = result.filter(item => item.id === productId); // get the existing product
 
+                    const updatedProductQuantity = parseInt((existingProduct[0].quantity || 0) + parseInt(quantity));
+                    
                     cartModel.updateProductToCart(userId, productId, updatedProductQuantity, (err, result) => {
-                        if (err) return res.status(500).send({ message: "Database Error" });
+                       
+                        if (err) return res.status(500).send(err.message);
 
                         return res.status(200).send({ message: `${quantity} more Item added in Cart` });
                     });
--- model/cartModel.js
+++ model/cartModel.js
@@ -2,16 +2,8 @@ const connection = require("../db");
 
 // get User's Product in Cart
 exports.getProductsInCart = (userId, callback) => {
-    connection.query("SELECT * FROM cart where userId = ?",
-        [userId], callback);
+    connection.query("Select product.id,product.title,product.min_price,cart.quantity,product.Image from cart Join product on cart.productId = product.id where cart.userId = ?", [userId], callback);
 }
-
-// get User's Product in Cart
-exports.getSpecificProductsInCart = (userId, productId, callback) => {
-    connection.query("SELECT * FROM cart where userId = ? AND productId = ?",
-        [userId, productId], callback);
-}
-
 
 // Insert Product to Cart
 exports.insertProductToCart = (userId, productId, quantity, callback) => {
